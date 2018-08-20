8b460138bc4ae3ead14f995fee602d6411fd0bdb
whyzp <920929030@qq.com>
2018-08-20 20:11:33 +0800

    二级分类页面完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -8,7 +8,7 @@ $(function(){
       data:{page:currentPage,pageSize:pageSize},
       dataType:'json',
       success:function(info){
-        console.log(info);  
+        // console.log(info);  
         var htmlStr = template('tpl',info);
         $('.lt-content tbody').html(htmlStr);
 
@@ -34,7 +34,7 @@ $(function(){
       data:{page:1,pageSize:100},
       dataType:'json',
       success:function(info){
-        console.log(info);
+        // console.log(info);
         var htmlStr = template('first_cate',info);
         $('#dropdown-menu').html(htmlStr);
       }
@@ -44,6 +44,8 @@ $(function(){
   $('#dropdown-menu').on('click','a',function(){
     var txt = $(this).text();
     $('.btn_text').text(txt);
+    $('[name="categoryId"]').val($(this).data('id'));
+    $('#form').data('bootstrapValidator').updateStatus('categoryId','VALID');
   })
 
   $("#updateImg").fileupload({
@@ -51,31 +53,63 @@ $(function(){
     done:function (e, data) {
       // console.log(data.result.picAddr);
       $('#imgBox img').attr('src',data.result.picAddr);
+      $('[name="brandLogo"]').val(data.result.picAddr);
+      $('#form').data('bootstrapValidator').updateStatus('brandLogo','VALID');
     }
   });
 
-  // $('#form').bootstrapValidator({
-  //   feedbackIcons:{
-  //     valid: 'glyphicon glyphicon-ok',
-  //     invalid: 'glyphicon glyphicon-remove',
-  //     validating: 'glyphicon glyphicon-refresh'
-  //   },
-  //   fields:{
+  $('#form').bootstrapValidator({
+    excluded: [],
+    feedbackIcons:{
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields:{
+      categoryId:{
+        validators:{
+          notEmpty:{
+            message:"请选择一级分类"
+          }
+        }
+      },
+      brandName:{
+        validators:{
+          notEmpty:{
+            message:"请输入二级分类"
+          }
+        }
+      },
+      brandLogo:{
+        validators:{
+          notEmpty:{
+            message:"请上传图片"
+          }
+        }
+      }
+    }
+  })
 
-  //   }
-  // })
-
-  // $('#submitBtn').click(function(){
-  //   $.ajax({
-  //     type:'post',
-  //     url:'/category/addSecondCategory',
-  //     data:{},
-  //     dataType:'json',
-  //     success:function(info){
-  //       console.log(info);
-        
-  //     }
-  //   })
-  // })
+  $('#form').on('success.form.bv',function(e){
+    // console.log($('#form').serialize());
+    
+    e.preventDefault();
+    $.ajax({
+      type:'post',
+      url:'/category/addSecondCategory',
+      data:$('#form').serialize(),
+      dataType:'json',
+      success:function(info){
+        if(info.success){
+          currentPage = 1;
+          render();
+          $('#addModal').modal('hide');
+          $('#form').data('bootstrapValidator').resetForm(true);
+          $('.dropdown .btn_text').text('请选择一级分类');
+          $('#imgBox img').attr('src','./images/none.png');
+        }
+      }
+    })
+  })
 
 })
